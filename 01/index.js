616f09a4a40d627935cd8570b05892971bf53794
kimura-apg <102777660+kimura-apg@users.noreply.github.com>
2025-04-15 10:56:07 +0900

    add sample code

---
+++ 01/index.js
@@ -0,0 +1,4 @@
+const sampleText = "abc";
+const sampleNumber = 123;
+
+console.log(sampleText > sampleNumber);
---
+++ 01/index.ts
@@ -0,0 +1,5 @@
+const sampleText = "abc"
+const sampleNumber = 123;
+
+console.log(sampleText > sampleNumber);
+
