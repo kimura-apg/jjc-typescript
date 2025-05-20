25bc4797bd6ea841885e3120d853046624871d67
kimura-apg <102777660+kimura-apg@users.noreply.github.com>
2025-05-20 04:38:34 +0000

    課題1の実装

---
+++ 03/taks1.js
@@ -0,0 +1,18 @@
+function removeDupulication1(array) {
+  const result = [];
+
+  array.forEach((item) => {
+    const isExist = result.includes(item);
+    console.log(isExist);
+
+    if (!isExist) {
+      result.push(item);
+    }
+  });
+
+  return result;
+}
+console.log(
+  removeDupulication1([1, 1, 1, 1, 2, 3, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5])
+);
+// 結果：[1, 2, 3, 4, 5]
