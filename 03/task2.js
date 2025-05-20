73102ed30233766b7a3d9afcbb31b67ede15afdd
中村 洸晴 <130522798+jjcnakamura@users.noreply.github.com>
2025-05-20 04:39:06 +0000

    課題２の実装

---
+++ 03/task2.js
@@ -0,0 +1,14 @@
+//文字列を逆順にする関数
+function Func(string) {
+  //文字列を配列に変換
+  const splitString = string.split("");
+  //配列を反転
+  const reverseArray = splitString.reverse();
+  //配列を結合して文字列に
+  const result = reverseArray.join("");
+
+  return result;
+}
+
+//引数で文字列を入れて関数を呼び出し
+console.log(Func("hello"));
